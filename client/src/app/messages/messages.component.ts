373d1d44a3282cd814afc1a1cc015527bfe6124e
Josh Burke <josh.burke@wtainc.com>
2021-09-08 11:27:08 -0400

    End of Section 15

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Message } from '../_models/message';
+import { Pagination } from '../_models/pagination';
+import { MessageService } from 'src/app/_services/message.service';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  messages: Message[] = [];
+  pagination: Pagination;
+  container = 'Unread';
+  pageNumber = 1;
+  pageSize = 5;
+  loading = false;
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.loadMessages();
   }
 
+  loadMessages() {
+    this.loading = true;
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe(response => {
+      this.messages = response.result;
+      this.pagination = response.pagination;
+      this.loading = false;
+    })
+  }
+
+  containerInit() {
+    this.pageNumber = 1;
+    this.loadMessages();
+  }
+
+  deleteMessage(id) {
+    this.messageService.deleteMessage(id).subscribe(() => {
+      this.messages.splice(this.messages.findIndex(m => m.id === id, 1));
+    })
+  }
+
+  pageChanged(event: any) {
+    if(event.page != this.pageNumber) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 }
